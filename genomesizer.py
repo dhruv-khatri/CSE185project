6d32977904e97ca9f142c4c4d5ad3792df85df5d
clingunis <clingunis.123@gmail.com>
2023-06-05 15:44:30 -0700

    Added Better Cutting Frequency and Mean Kmer Coverage Algorithm

--- genomesizer.py
+++ genomesizer.py
@@ -201,8 +201,10 @@ with open(input_file, 'r') as file:
            and trimmed_sorted_histogram[frequency+1]<trimmed_sorted_histogram[frequency+2]):
             cut_freq=frequency
             break
-    if (cut_freq>50):
-        cut_freq=1
+    # TODO make max_cut_freq an option
+    max_cut_freq=5
+    if (cut_freq>1000 or cut_freq>max_cut_freq):
+        cut_freq=max_cut_freq
     print(cut_freq)
     error_kmers=0
     if(not cut_freq==-1):
@@ -211,20 +213,23 @@ with open(input_file, 'r') as file:
             trimmed_sorted_histogram.pop(i)
 
     min_key=min(list(trimmed_sorted_histogram.keys()))
+    max_value = max(trimmed_sorted_histogram.values())
+    max_k = [key for key, value in trimmed_sorted_histogram.items() if value == max_value][0]
+    mean_kmer_coverage=max_k
     # Walk along histogram to get mean kmer coverage
-    for frequency in range(min_key, max_key-9):
-        if(trimmed_sorted_histogram[frequency]>trimmed_sorted_histogram[frequency+1]
-           and trimmed_sorted_histogram[frequency+1]>trimmed_sorted_histogram[frequency+2]
-           and trimmed_sorted_histogram[frequency+2]>trimmed_sorted_histogram[frequency+3]
-           and trimmed_sorted_histogram[frequency+3]>trimmed_sorted_histogram[frequency+4]
-           and trimmed_sorted_histogram[frequency+4]>trimmed_sorted_histogram[frequency+5]
-           and trimmed_sorted_histogram[frequency+5]>trimmed_sorted_histogram[frequency+6]
-           and trimmed_sorted_histogram[frequency+6]>trimmed_sorted_histogram[frequency+7]
-           and trimmed_sorted_histogram[frequency+7]>trimmed_sorted_histogram[frequency+8]
-           and trimmed_sorted_histogram[frequency+8]>trimmed_sorted_histogram[frequency+9]
-           and trimmed_sorted_histogram[frequency+9]>trimmed_sorted_histogram[frequency+10]):
-            mean_kmer_coverage=frequency
-            break
+    #for frequency in range(min_key, max_key-9):
+    #    if(trimmed_sorted_histogram[frequency]>trimmed_sorted_histogram[frequency+1]
+    #       and trimmed_sorted_histogram[frequency+1]>trimmed_sorted_histogram[frequency+2]
+    #       and trimmed_sorted_histogram[frequency+2]>trimmed_sorted_histogram[frequency+3]
+    #       and trimmed_sorted_histogram[frequency+3]>trimmed_sorted_histogram[frequency+4]
+    #       and trimmed_sorted_histogram[frequency+4]>trimmed_sorted_histogram[frequency+5]
+    #       and trimmed_sorted_histogram[frequency+5]>trimmed_sorted_histogram[frequency+6]
+    #       and trimmed_sorted_histogram[frequency+6]>trimmed_sorted_histogram[frequency+7]
+    #       and trimmed_sorted_histogram[frequency+7]>trimmed_sorted_histogram[frequency+8]
+    #       and trimmed_sorted_histogram[frequency+8]>trimmed_sorted_histogram[frequency+9]
+    #       and trimmed_sorted_histogram[frequency+9]>trimmed_sorted_histogram[frequency+10]):
+    #        mean_kmer_coverage=frequency
+    #        break
 
     num_kmers_obs=0
     for frequency in range(cut_freq+1,max_key+1):
