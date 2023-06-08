c71a874285106a5b3fc03a807350286d24382ca9
dhruv-khatri <134110316+dhruv-khatri@users.noreply.github.com>
2023-06-07 22:47:57 -0700

    Update reverse_complement.py

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
 def reverse_complement(sequence):
+    
+    """
+    Function to generate the reverse complement of a DNA sequence.
+
+    Arguments:
+    - sequence: a string representing a DNA sequence
+
+    Returns:
+    - reverse_complement_seq: a string representing the reverse complement of the input sequence
+    """
+    
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     reverse_seq = sequence[::-1]
     reverse_complement_seq = ''.join(complement.get(base, base) for base in reverse_seq)
